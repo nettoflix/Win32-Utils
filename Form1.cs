19907d4782aa6e9b6c2b4ce60c062540960f4308
unknown <netto2002.sjb@gmail.com>
2022-04-08 08:15:00 -0300

    Added Bot

---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Utils;
+namespace testAI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+            //MemoryUtils.SendKeyToWindow("RiotWindowClass", MemoryUtils.VK_A);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //IntPtr hWnd = MemoryUtils.FindWindow("Qt5QWindowOwnDCIcon", null);
+            //MemoryUtils.ShowWindow(hWnd, 3);
+            //MemoryUtils.SetForegroundWindow(hWnd);
+            //int result = MemoryUtils.SendKeyToWindow("Qt5QWindowOwnDCIcon", MemoryUtils.VK_Q);
+            IntPtr hWnd = MemoryUtils.FindWindow("RiotWindowClass", null);
+            MemoryUtils.ShowWindow(hWnd, 3);
+            MemoryUtils.SetForegroundWindow(hWnd);
+
+            int result = MemoryUtils.SendKeyToWindow("RiotWindowClass", MemoryUtils.VK_D);
+            Thread.Sleep(500);
+            MemoryUtils.Input[] kb_inputs = new MemoryUtils.Input[]
+            {
+                new MemoryUtils.Input
+                {
+                    type = (int)MemoryUtils.InputType.Keyboard,
+                    u = new MemoryUtils.InputUnion
+                    {
+                        ki = new MemoryUtils.KeyboardInput
+                        {
+                            wVk = 0, //wVk would be the virtual key code but it is being ignore because the Scancode flag is set
+                            wScan = 0x11, // W -- that's the hardware key code
+                            dwFlags = (uint)(MemoryUtils.KeyEventF.KeyDown | MemoryUtils.KeyEventF.Scancode),
+                            dwExtraInfo = MemoryUtils.GetMessageExtraInfo()
+                        }
+                    }
+                },
+                new MemoryUtils.Input
+                {
+                    type = (int)MemoryUtils.InputType.Keyboard,
+                    u = new MemoryUtils.InputUnion
+                    {
+                        ki = new MemoryUtils.KeyboardInput
+                        {
+                            wVk = 0,
+                            wScan = 0x11, // W
+                            dwFlags = (uint)(MemoryUtils.KeyEventF.KeyUp | MemoryUtils.KeyEventF.Scancode),
+                            dwExtraInfo = MemoryUtils.GetMessageExtraInfo()
+                        }
+                    }
+                }
+            };
+                MemoryUtils.SendInput((uint)kb_inputs.Length, kb_inputs, Marshal.SizeOf(typeof(MemoryUtils.Input)));
+            //MOUSE 
+            MemoryUtils.Input[] ms_inputs = new MemoryUtils.Input[]
+            {
+                new MemoryUtils.Input
+                {
+                    type = (int) MemoryUtils.InputType.Mouse,
+                    u = new MemoryUtils.InputUnion
+                    {
+                        mi = new MemoryUtils.MouseInput
+                        {
+                            dx = 300,
+                            dy = 100,
+                            dwFlags = (uint)(MemoryUtils.MouseEventF.Move | MemoryUtils.MouseEventF.RightDown),
+                            dwExtraInfo = MemoryUtils.GetMessageExtraInfo()
+                        }
+                    }
+                },
+                new MemoryUtils.Input
+                {
+                    type = (int) MemoryUtils.InputType.Mouse,
+                    u = new MemoryUtils.InputUnion
+                    {
+                        mi = new MemoryUtils.MouseInput
+                        {
+                            dwFlags = (uint)MemoryUtils.MouseEventF.RightUp,
+                            dwExtraInfo = MemoryUtils.GetMessageExtraInfo()
+                        }
+                    }
+                }
+            };
+            MemoryUtils.SendInput((uint)ms_inputs.Length, ms_inputs, Marshal.SizeOf(typeof(MemoryUtils.Input)));
+        }
+
+
+
+    }
+}
---
+++ MemoryUtils.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public static class MemoryUtils
+    {
+        public const uint WM_KEYDOWN = 0x0100;
+        public const uint WM_KEYUP = 0x0101;
+        public const uint VK_SPACE = 0x20;
+        public const uint VK_N = 0x4E;
+        public const uint VK_F1 = 0x70;
+        public const uint VK_F2 = 0x71;
+        public const uint VK_F3 = 0x72;
+        public const uint VK_F4 = 0x73;
+        public const uint VK_F5 = 0x74;
+        public const uint VK_F6 = 0x75;
+        public const uint VK_F7 = 0x76;
+        public const uint VK_F8 = 0x77;
+        public const uint VK_F9 = 0x78;
+        public const uint VK_F10 = 0x79;
+        public const uint VK_F11 = 0x7A;	
+        public const uint VK_F12= 0x7B;
+        public const uint VK_A = 0x41;
+        public const uint VK_Q = 0x51;
+        public const uint VK_D = 0x44;
+        [StructLayout(LayoutKind.Sequential)]
+        public struct KeyboardInput
+        {
+            public ushort wVk;
+            public ushort wScan;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MouseInput
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct HardwareInput
+        {
+            public uint uMsg;
+            public ushort wParamL;
+            public ushort wParamH;
+        }
+        [StructLayout(LayoutKind.Explicit)]
+        public struct InputUnion
+        {
+            [FieldOffset(0)] public MouseInput mi;
+            [FieldOffset(0)] public KeyboardInput ki;
+            [FieldOffset(0)] public HardwareInput hi;
+        }
+        public struct Input
+        {
+            public int type;
+            public InputUnion u;
+        }
+        [Flags]
+        public enum InputType
+        {
+            Mouse = 0,
+            Keyboard = 1,
+            Hardware = 2
+        }
+        [Flags]
+        public enum KeyEventF
+        {
+            KeyDown = 0x0000,
+            ExtendedKey = 0x0001,
+            KeyUp = 0x0002,
+            Unicode = 0x0004,
+            Scancode = 0x0008
+        }
+        [Flags]
+        public enum MouseEventF
+        {
+            Absolute = 0x8000,
+            HWheel = 0x01000,
+            Move = 0x0001,
+            MoveNoCoalesce = 0x2000,
+            LeftDown = 0x0002,
+            LeftUp = 0x0004,
+            RightDown = 0x0008,
+            RightUp = 0x0010,
+            MiddleDown = 0x0020,
+            MiddleUp = 0x0040,
+            VirtualDesk = 0x4000,
+            Wheel = 0x0800,
+            XDown = 0x0080,
+            XUp = 0x0100
+        }
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, String lpWindowName);
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int wMsg, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint SendInput(uint nInputs, Input[] pInputs, int cbSize);
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetMessageExtraInfo();
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        public static int ReadMemoryWithOffsets(IntPtr processHandle, int BaseAddress, int[] Offsets)
+        {
+            int bytesRead = 0;
+            byte[] bufferOut = new byte[4];
+            int address = BaseAddress;
+            foreach (int offset in Offsets)
+            {
+                ReadProcessMemory((int)processHandle, address, bufferOut, bufferOut.Length, ref bytesRead);
+                address = BitConverter.ToInt32(bufferOut,0) + offset;
+            }
+            ReadProcessMemory((int)processHandle, address, bufferOut, bufferOut.Length, ref bytesRead);
+            return BitConverter.ToInt32(bufferOut, 0);
+        }
+        public static int SendKeyToWindow(string wndName, uint keyCode)
+        {
+            string lpClassName = wndName; //taleon window class name
+            IntPtr hwnd = FindWindow(lpClassName, null);
+            return SendMessage(hwnd, (int)WM_KEYDOWN, (IntPtr)keyCode, (IntPtr)0);
+        }
+
+        public static uint getKeyCodeFromString(string str)
+        {
+            switch (str)
+            {
+                case "F1":
+                    return VK_F1;
+                    break;
+
+                case "F2":
+                    return VK_F2;
+                    break;
+                case "F3":
+                    return VK_F3;
+                    break;
+                case "F4":
+                    return VK_F4;
+                    break;
+                case "F5":
+                    return VK_F5;
+                    break;
+                case "F6":
+                    return VK_F6;
+                case "F7":
+                    return VK_F7;
+                case "F8":
+                    return VK_F8;
+                case "F9":
+                    return VK_F9;
+                case "F10":
+                    return VK_F10;
+                case "F11":
+                    return VK_F11;
+                case "F12":
+                    return VK_F12;
+                    break;
+
+                default:
+                    return 0;
+                    break;
+            }
+        }
+    }
+}
